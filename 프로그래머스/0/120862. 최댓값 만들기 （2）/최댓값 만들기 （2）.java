7610ed28ecc2191b31b6ea9ebbfc766bd0a5731f
dlgusals1-one <s2630@e-mirim.hs.kr>
2026-07-01 13:22:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.23 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = 0; j < numbers.length-1; j++){
+                if(numbers[j] > numbers[j+1]){
+                    int temp = numbers[j+1];
+                    numbers[j+1] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        int answer = numbers[numbers.length-1]*numbers[numbers.length-2];
+        if(numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2]){
+            answer = numbers[0]*numbers[1];
+        }
+        return answer;
+    }
+}
