58059713c59560d63ae420d2d72e98ab80578468
dlgusals1-one <s2630@e-mirim.hs.kr>
2026-08-17 22:22:44 +0900

    [level 0] Title: 5명씩, Time: 0.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,15 +1,18 @@
 class Solution {
     public String[] solution(String[] names) {
-        int count = 0;
-        for(int i = 0; i < names.length; i++){
-            if(i%5==0) count++;
-        }
-        String[] answer = new String[count];
+        // int count = 0;
+        // for(int i = 0; i < names.length; i++){
+        //     if(i%5==0) count++;
+        // }
+        // String[] answer = new String[count];
+        // int idx = 0;
+        // for(int i = 0; i < answer.length; i++){
+        //     answer[i] = names[idx];
+        //     idx += 5;
+        // }
+        String[] answer = new String[names.length%5 == 0 ? names.length/5 : names.length/5 + 1];
         int idx = 0;
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = names[idx];
-            idx += 5;
-        }
+        for(int i = 0; i < answer.length; i++, idx += 5) answer[i] = names[idx];
         return answer;
     }
 }
