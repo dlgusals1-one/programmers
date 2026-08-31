6d1579a440ad5154f066586cb26b6caa3c120923
dlgusals1-one <s2630@e-mirim.hs.kr>
2026-08-31 19:45:03 +0900

    [level 0] Title: ad 제거하기, Time: 0.49 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(String[] strArr) {
+        int count = 0;
+        for(int i = 0; i < strArr.length; i++){
+            if(strArr[i].contains("ad")) count++;
+        }
+        String[] answer = new String[strArr.length-count];
+        int idx = 0;
+        for(int i = 0; i < strArr.length; i++){
+            if(!strArr[i].contains("ad")) {
+                answer[idx] = strArr[i];
+                idx++;
+            }
+        }   
+        return answer;
+    }
+}
