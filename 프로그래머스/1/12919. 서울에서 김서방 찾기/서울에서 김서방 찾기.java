5413535848cd94565738038bc33400d13c0740bb
dlgusals1-one <s2630@e-mirim.hs.kr>
2026-09-07 08:51:15 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.79 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int count = 0;
+        for(String str:seoul){
+            if(str.equals("Kim")) {
+                answer = "김서방은 " + count + "에 있다";
+                break;
+        }
+            count++;
+        }
+        return answer;
+    }
+}
